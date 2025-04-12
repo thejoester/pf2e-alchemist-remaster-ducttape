2f778699cffd316c64dfbb23ce743542ead624f5
PandaPlex <Jordan.b.ireland@gmail.com>
2025-04-11 22:09:00 -0700

    updated for healing bomb

---
+++ scripts/HealingBomb.js
@@ -0,0 +1,6 @@
+import { debugLog, getSetting, hasFeat, isAlchemist, hasActiveOwners  } from './settings.js';
+import { LOCALIZED_TEXT } from "./localization.js";
+
+async function craftHealingBomb(actor, selectedUuid, dbSelectedUuid, itemType, selectedType, specialIngredient) {
+
+}
